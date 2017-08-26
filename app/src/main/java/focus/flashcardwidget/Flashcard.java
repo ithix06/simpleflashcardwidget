6285ca6c40ce163f5ee880f60afbbeb954abfcc5
Neal Roessler <groessler24@gmail.com>
2017-08-26 14:54:43 -0700

    adding star

--- app/src/main/java/focus/flashcardwidget/Flashcard.java
+++ app/src/main/java/focus/flashcardwidget/Flashcard.java
@@ -4,9 +4,39 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Flashcard {
     private String front;
     private String back;
     private boolean displayFront = true;
+    private boolean starred = false;
+
+    public Flashcard(String front, String back, boolean displayFront) {
+        this.front = front;
+        this.back = back;
+        this. displayFront = displayFront;
+    }
+
+    public void setDisplayFront(boolean toDisplay) {
+        this.displayFront = toDisplay;
+    }
+
+    public boolean isDisplayFront() {
+        return displayFront;
+    }
+
+    public void setStarred(boolean isStarred) {
+        starred = isStarred;
+    }
+
+    public boolean isStarred() {
+        return this.starred;
+    }
+
+    public String getFront() {
+        return front;
+    }
+
+    public String getBack() {
+        return back;
+    }
 }
--- app/src/main/java/focus/flashcardwidget/SimpleWidgetProvider.java
+++ app/src/main/java/focus/flashcardwidget/SimpleWidgetProvider.java
@@ -8,8 +8,10 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.widget.ImageButton;
 import android.widget.RemoteViews;
 
 import java.io.BufferedReader;
@@ -19,6 +21,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -32,16 +35,20 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
     private static ArrayList<Flashcard> wordsList = new ArrayList<>();
     private static boolean firstTime = true;
     private static int wordCount = 0;
+    private static String buttonkey = "a";
 
 
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
+        boolean star = true;
+
         if (firstTime == true) {
             firstTime = false;
             try {
                 wordsList = readFile();
+                Collections.shuffle(wordsList);
             } catch (Exception e) {
                 throw new RuntimeException(e.getMessage());
             }
@@ -49,16 +56,24 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
 
         final int count = appWidgetIds.length;
 
+
+
         for (int i = 0; i < count; i++) {
             int widgetId = appWidgetIds[i];
             //String number = String.format("%03d", (new Random().nextInt(900) + 100));
             Flashcard currentFlashcard;
             if(wordsList.get(wordCount).isDisplayFront()) {
                 currentFlashcard = wordsList.get(wordCount);
-                wordsList.get(wordCount).setDisplayFront(false);
+                
+                if ( buttonkey != null && buttonkey.equals("next")) {
+                    wordsList.get(wordCount).setDisplayFront(false);
+                }
                 //wordsList.remove(0)
             } else {
-                wordCount ++;
+
+                if ( buttonkey != null && buttonkey.equals("next")) {
+                    wordCount++;
+                }
 
                 if(wordCount >= wordsList.size()) {
                     flipToFront(wordsList);
@@ -73,16 +88,104 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
                     R.layout.simple_widget);
             remoteViews.setTextViewText(R.id.textView, displayText);
 
+
+
+            if ( buttonkey != null && buttonkey.equals("star")) {
+                currentFlashcard.setStarred(!currentFlashcard.isStarred());
+                if (currentFlashcard.isStarred()) {
+                    remoteViews.setImageViewResource(R.id.toggleStar, R.drawable.staron);
+                } else {
+                    remoteViews.setImageViewResource(R.id.toggleStar, R.drawable.staroff);
+                }
+
+                Intent intent = new Intent(context, SimpleWidgetProvider.class);
+
+                intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+                intent.putExtra("buttonkey", "next");
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                        0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+
+                // Register an onClickListener for 2nd button............
+                Intent intent2 = new Intent(context, SimpleWidgetProvider.class);
+
+                intent2.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                intent2.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+                intent2.putExtra("buttonkey", "star");
+
+                PendingIntent pendingIntent2 = PendingIntent.getBroadcast(context,
+                        1, intent2, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                remoteViews.setOnClickPendingIntent(R.id.toggleStar, pendingIntent2);
+
+                appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
+                return;
+
+            }
+
+            // update star image regardless
+            if (currentFlashcard.isStarred()) {
+                remoteViews.setImageViewResource(R.id.toggleStar, R.drawable.staron);
+            } else {
+                remoteViews.setImageViewResource(R.id.toggleStar, R.drawable.staroff);
+            }
+
+//            Intent intent = new Intent(context, SimpleWidgetProvider.class);
+//            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+//            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+//            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+//                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+//
+//
+//
+//
+//            remoteViews.setOnClickPendingIntent(R.id.toggleStar, pendingIntent);
+//            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
+            // Register an onClickListener for 1st button
             Intent intent = new Intent(context, SimpleWidgetProvider.class);
+
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            intent.putExtra("buttonkey", "next");
+
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
                     0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
             remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+
+            // Register an onClickListener for 2nd button............
+            Intent intent2 = new Intent(context, SimpleWidgetProvider.class);
+
+            intent2.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent2.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            intent2.putExtra("buttonkey", "star");
+
+            PendingIntent pendingIntent2 = PendingIntent.getBroadcast(context,
+                    1, intent2, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            remoteViews.setOnClickPendingIntent(R.id.toggleStar, pendingIntent2);
+
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
-
-
         }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle b =intent.getExtras();
+        try {
+            buttonkey = b.getString("buttonkey");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        super.onReceive(context, intent);
+
     }
 
     private void flipToFront(ArrayList<Flashcard> cards) {
